c6ebcc7a8e47bf63f2b12953f7ead8eaf488a82e
Daniil <daniilrizhenko@gmail.com>
2025-05-17 15:53:34 +0300

    add project root to sys.path

--- parser/parser.py
+++ parser/parser.py
@@ -3,6 +3,9 @@ import datetime
 
 import logging
 import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import requests
 from bs4 import BeautifulSoup, Tag
